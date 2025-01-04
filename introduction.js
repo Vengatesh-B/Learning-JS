c91ea68c3a76398fbf9bc9d547a404ced089d1dc
vengatesh <vengateshb28@gmail.com>
2025-01-04 13:24:56 +0530

    (feat: add content)

--- introduction.js
+++ introduction.js
@@ -192,3 +192,31 @@ else {
   reply = 'course not available';
   console.log(reply);
 }
+
+// nested if
+
+let testScore = 6;
+let grade;
+let collegeStudent = true;
+
+if(testScore >= 90) {
+  grade = 'A';
+}
+else if (testScore >= 80) {
+  grade = 'B';
+}
+else if (testScore >= 70) {
+  grade = 'C';
+}
+else if (testScore >= 60) {
+  grade = 'D';
+}
+else {
+  if (collegeStudent) {
+    grade = 'RA';
+  }
+  else {
+    grade = 'F'
+  }
+}
+console.log(grade);

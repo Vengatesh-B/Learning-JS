ad1fe6b696ef2c3939465a398c63f90fc72b2266
vengatesh <vengateshb28@gmail.com>
2025-02-12 11:04:06 +0530

    (feat:dom concept)

--- jsdom.js
+++ jsdom.js
@@ -1,13 +1,19 @@
 // DOM - Document Object Model
 
+// select the element using their id
 const view1 = document.getElementById("view1")
 console.log(view1);
 
 const view2 = document.querySelector("#view2");
 console.log(view2);
 
-view1.style.display = "flex";
+view1.style.flexDirection = "row";
 view2.style.display = "flex";
 
+
+// select the element using their class names
 const views = document.getElementsByClassName("view");
-console.log(views);
+console.log(views); // it gives the html collection
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews); // it gives the nodelist, it includes all the contents
